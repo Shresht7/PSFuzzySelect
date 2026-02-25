4938b038d38a098d1a7aa6c75b8c6aec4ec58edf
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-02-25 22:39:04 +0530

    Optimize `ConsoleRenderer` to batch updates and minimize console writes

--- UI/Renderer/ConsoleRenderer.cs
+++ UI/Renderer/ConsoleRenderer.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 using PSFuzzySelect.UI.Helpers;
 
 namespace PSFuzzySelect.UI.Renderer;
@@ -22,8 +24,14 @@ public class ConsoleRenderer
 
     public void Render(FrameBuffer currentBuffer)
     {
+        // The final string representation of the frame to be rendered to the console
+        var frame = new StringBuilder();
+
         for (int y = 0; y < currentBuffer.Height; y++)
         {
+            int runStart = -1;
+            Style activeStyle = Style.Default;
+
             for (int x = 0; x < currentBuffer.Width; x++)
             {
                 Cell currentCell = currentBuffer.GetCell(x, y);
@@ -32,19 +40,54 @@ public class ConsoleRenderer
                 // Only update the cell if it has changed since the last render
                 if (currentCell != previousCell)
                 {
-                    // !! This is temporary. Eventually we want to batch updates together and minimize the number of cursor movements and writes to the console for better performance.
-                    Console.SetCursorPosition(x, y);
-                    if (currentCell.Style != null)
+                    // Start the first run if uninitialized
+                    if (runStart == -1)
                     {
-                        Console.Write(currentCell.Style?.ToAnsi());
+                        runStart = x;
+                        activeStyle = currentCell.Style ?? Style.Default;
+                        frame.Append(Ansi.CursorPosition(y, x));
+                        frame.Append(activeStyle.ToAnsi());
                     }
-                    Console.Write(currentCell.Character);
-                    if (currentCell.Style != null)
+
+                    // Update the active style if it changes since the last cell
+                    if (currentCell.Style != activeStyle)
                     {
-                        Console.Write(Ansi.Reset);
+                        if (currentCell.Style != null)
+                        {
+                            // ? These null-checks are getting annoying. Maybe something can be done about this.
+                            frame.Append(Ansi.Reset);
+                            activeStyle = currentCell.Style ?? Style.Default;
+                            frame.Append(activeStyle.ToAnsi());
+                        }
+                    }
+
+                    // Append the character to the frame
+                    frame.Append(currentCell.Character);
+                }
+                else
+                {
+                    // If the cell is the same as the previous render, we can skip it and reset the run if it was active
+                    if (runStart != -1)
+                    {
+                        frame.Append(Ansi.Reset);
+                        runStart = -1;
+                        activeStyle = Style.Default;
                     }
                 }
             }
+
+            // Reset the style at the end of the line if it was changed
+            if (runStart != -1)
+            {
+                frame.Append(Ansi.Reset);
+            }
+        }
+
+        // Write the final frame to the console
+        if (frame.Length > 0)
+        {
+            Console.Out.Write(frame.ToString());
+            Console.Out.Flush();
         }
 
         // Update the previous buffer reference to the current buffer for the next render cycle
