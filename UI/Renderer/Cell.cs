07278c050f45df00684398e0f234af426fbc9349
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-02-26 16:39:32 +0530

    Make `Style` non-optional
    Saves me the headache of having to do null-checks everywhere

--- UI/Renderer/Cell.cs
+++ UI/Renderer/Cell.cs
@@ -3,17 +3,17 @@ using PSFuzzySelect.UI.Styles;
 namespace PSFuzzySelect.UI.Renderer;
 
 /// <summary>
-/// Represents a single cell in the terminal output, containing a character and optional ANSI style information
+/// Represents a single cell in the terminal output, containing a character and its associated styles
 /// </summary>
 /// <param name="character">The character to be displayed in the cell</param>
-/// <param name="style">An optional Style instance representing the ANSI styles</param>
-public struct Cell(char character, Style? style = null)
+/// <param name="style">The style to apply to the character</param>
+public struct Cell(char character, Style style)
 {
     /// <summary>The character to be displayed in the cell</summary>
     public char Character { get; set; } = character;
 
-    /// <summary>An optional Style instance representing the ANSI styles</summary>
-    public Style? Style { get; set; } = style;
+    /// <summary>The style to apply to the character</summary>
+    public Style Style { get; set; } = style;
 
     // Equality members to allow comparison of Cell instances based on their content
     public override bool Equals(object? obj) => obj is Cell cell && Equals(cell);
