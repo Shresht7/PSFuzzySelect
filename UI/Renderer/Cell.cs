eec0a25495f2f78abb1ee7b18ba4d467aa12109a
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-02-26 16:41:17 +0530

    Make `Cell` struct readonly and immutable
    This ensures immutability and prevents accidental modification of cell data

--- UI/Renderer/Cell.cs
+++ UI/Renderer/Cell.cs
@@ -7,13 +7,13 @@ namespace PSFuzzySelect.UI.Renderer;
 /// </summary>
 /// <param name="character">The character to be displayed in the cell</param>
 /// <param name="style">The style to apply to the character</param>
-public struct Cell(char character, Style style)
+public readonly struct Cell(char character, Style style)
 {
     /// <summary>The character to be displayed in the cell</summary>
-    public char Character { get; set; } = character;
+    public char Character { get; init; } = character;
 
     /// <summary>The style to apply to the character</summary>
-    public Style Style { get; set; } = style;
+    public Style Style { get; init; } = style;
 
     // Equality members to allow comparison of Cell instances based on their content
     public override bool Equals(object? obj) => obj is Cell cell && Equals(cell);
