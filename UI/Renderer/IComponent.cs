b9f2d6dd7695563ad72baebaf5bb532c849a43a1
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-02-26 21:18:10 +0530

    Create `IComponent` interface to describe a renderable component

---
+++ UI/Renderer/IComponent.cs
@@ -0,0 +1,11 @@
+namespace PSFuzzySelect.UI.Renderer;
+
+public interface IComponent
+{
+    /// <summary>
+    /// Renders the component onto the provided surface.
+    /// The surface represents a portion of the terminal output where the component should draw itself.
+    /// </summary>
+    /// <param name="surface">The surface to render the component on</param>
+    void Render(ISurface surface);
+}
